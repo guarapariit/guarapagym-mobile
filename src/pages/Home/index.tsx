ceaef65bc6c5f9c3e06dd9a6f3fc5f0ac441bbc6
Bruno Rodrigues <brunorodriguesricardo@gmail.com>
2021-03-04 08:22:03 -0300

    Initial commit 🎉

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import { View } from 'react-native';
+
+import { Container, Title } from './styles';
+
+const Home: React.FC = () => {
+  return (
+    <Container>
+      <Title>Hello world!</Title>
+    </Container>
+  );
+};
+
+export default Home;
