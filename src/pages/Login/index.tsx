5356c4c65303462e8062743348992c2be06f931e
YtaloWill <ytalowillgloria@gmail.com>
2021-06-16 03:48:20 -0300

    login complete

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,11 +1,46 @@
-import React from 'react';
-import { Text, Image } from 'react-native'
+import { useNavigation } from '@react-navigation/native';
+import React, { useState } from 'react';
+import { Text, Image, Alert } from 'react-native'
 import { logoBlack } from '../../assets';
+import api from '../../services/api';
 import { Colors } from '../../utils/ColorsEnum';
 
 import { Container, Header, LoginButton, LoginButtonContainer, Form, Input, InputContainer } from './styles';
 
 const Login: React.FC = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  
+  const navigation = useNavigation();
+
+  async function handleNavigate(): Promise<void> {
+    navigation.navigate('Home', {
+      screen: 'Home',
+    });
+  }
+
+  function handleEmailChange(email: string) {
+    setEmail(email);
+  }
+
+  function handlePasswordChange(password: string) {
+    setPassword(password);
+  }
+
+  async function signIn() {
+    const data = {
+      email, 
+      password
+    }
+    try {
+      const request = await api.post('https://guarapagym.herokuapp.com/sessions', data)
+      console.log(request)
+      return !!request.data.token && request.data.token !== ''
+    } catch (err) {
+      Alert.alert('Erro ao logar, tente novamente mais tarde.');
+    }
+  }
+
   return (
     <Container>
       <Header>
@@ -15,7 +50,6 @@ const Login: React.FC = () => {
             width: 250,
             height: 120,
             alignSelf: 'center',
-            
           }}
         />
       </Header>
@@ -35,17 +69,25 @@ const Login: React.FC = () => {
             accessibilityLabel='Email'
             placeholder='Email'
             placeholderTextColor={Colors.BLACK}
+            value={email}
+            autoCorrect={false}
+            onChangeText={e => handleEmailChange(e)}
           />
           <Input
             accessibilityLabel='Senha'
             placeholder='Senha'
             placeholderTextColor={Colors.BLACK}
+            value={password}
+            autoCorrect={false}
             secureTextEntry={true}
+            onChangeText={e => handlePasswordChange(e)}
           />
         </InputContainer>
       </Form>
       <LoginButtonContainer
-        onPress={() => {}}
+        onPress={() => {
+          !!signIn() ? handleNavigate() : Alert.alert('Verifique seu email/senha')
+        }}
         accessibilityLabel="Entrar"
       >
         <LoginButton>Entrar</LoginButton>
